47118df03a6e6ee7c4321609048f59574a282e07
Bob Cook <bobcook@sunrem.com>
2012-05-03 15:48:19 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wescor::Application.config.secret_token = '450dcdd01282babbc276ae7fcfe657cdb6da5748c7af476ab9bbb5f7c34dd356c702d123d3b0a67dee12d9e140cd388ec8a5e37c7aaae061935dc1a4fc088a80'
